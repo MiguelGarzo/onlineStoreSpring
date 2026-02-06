ed932179f5d0ad54f82b3bc48352348003cf2577
MiguelGarzo <mgarzom@uoc.edu>
2026-02-06 16:25:38 +0100

    stripe

--- tienda-frontend/src/index.js
+++ tienda-frontend/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 
-const stripePromise = loadStripe("YOUR_PUBLICABLE_KEY");
+const stripePromise = loadStripe("YOUR_PUBLICABLE_STRIPE_KEY");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
